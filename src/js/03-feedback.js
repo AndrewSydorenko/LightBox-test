31adac8674a42852ae4babbfc7b8437a43a5e86c
AndrewSydorenko <samtaktreba@gmail.com>
2023-02-16 16:36:21 +0200

    final final draft

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,7 +2,7 @@ import throttle from 'lodash.throttle';
 
 const formEl = document.querySelector('.feedback-form');
 const STORAGE_KEY = 'feedback-form-state'
-const formData = {};
+const formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
 
 formEl.addEventListener('submit', onFormSubmit);
 formEl.addEventListener('input', throttle(onFormInput, 500));
@@ -19,7 +19,11 @@ function onFormSubmit(event) {
     const { target } = event;
     const { email, message } = target.elements;
     const mail = email.value;
-    const text = message.value
+    const text = message.value;
+    if (!mail || !text) {
+        alert('пусто')
+        return
+    }
     console.log({ mail, text });
     event.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY);
@@ -27,7 +31,7 @@ function onFormSubmit(event) {
 
 const textOutput = ({ elements }) => {
 
-    const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+    const savedMessage = formData;
     if (!savedMessage) {
         return
     }
