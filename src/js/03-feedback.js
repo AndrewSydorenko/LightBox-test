f0b699559699cc59b632bb62cc683e08c0ff1715
AndrewSydorenko <samtaktreba@gmail.com>
2023-02-18 15:45:47 +0200

    add blur

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,14 +4,28 @@ const formEl = document.querySelector('.feedback-form');
 const STORAGE_KEY = 'feedback-form-state'
 const formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
 
+formEl.querySelectorAll('input').forEach(input => {
+    input.addEventListener('blur', onInputBlur);
+});
+
 formEl.addEventListener('submit', onFormSubmit);
 formEl.addEventListener('input', throttle(onFormInput, 500));
 
+function onInputBlur(event) {
+    // save only the field that was changed
+    const field = event.target.name;
+    const value = event.target.value;
+    formData[field] = value;
+    const formJson = JSON.stringify(formData);
+    localStorage.setItem(STORAGE_KEY, formJson);
+}
+
 
 function onFormInput(event) {
+
     formData[event.target.name] = event.target.value;
     const formJson = JSON.stringify(formData);
-    localStorage.setItem(STORAGE_KEY, formJson)
+    localStorage.setItem(STORAGE_KEY, formJson);
 };
 
 function onFormSubmit(event) {
